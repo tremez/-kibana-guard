7dfcb72bd4bf41344fa4e46f695f110b3ee38c69
Taras Remez <taras.remez@telix.com.ua>
2016-12-02 00:58:42 +0200

    switch to passport, change structure

---
+++ guardproxy.js
@@ -0,0 +1,123 @@
+/**
+ * Created by taras on 12/2/16.
+ */
+var config = require('./config.json');
+var users=config.users;
+var proxy;
+
+function getPrefix(req) {
+	var prefix;
+	var user=req.user;
+	if (user) {
+		prefix = users[user.username];
+	}else{
+		prefix="JOPA";
+	}
+	return prefix;
+}
+function isIndexName(indexName) {
+	if (indexName[0] === '_') {
+		return false;
+	}
+	return true;
+}
+function isReplacementNeeded(index) {
+	if (index.indexOf('asteriskcdrmain-') !== -1) {
+		return true;
+	}
+	if (index.indexOf('asteriskcdr-') !== -1) {
+		return true;
+	}
+	if (index.indexOf('asteriskqueuelog-') !== -1) {
+		return true;
+	}
+	return false;
+
+}function prepareIndexName(oldIndexName, prefix) {
+
+	if (!isReplacementNeeded(oldIndexName)) {
+		return oldIndexName;
+	}
+	var indexNameParts = oldIndexName.split('-*');
+	indexNameParts[0] = indexNameParts[0] + prefix;
+	var indexName = indexNameParts.join('-*');
+	return indexName;
+}
+function prepareMgetBody(body, prefix) {
+	var docs = body.docs;
+	var newBody = {};
+	var newDocs = docs.map(function (el) {
+		el._index = prepareIndexName(el._index, prefix);
+		el._id = prepareIndexName(el._id, prefix)
+		return el;
+	});
+	newBody.docs = newDocs;
+	return newBody;
+}
+function onProxyReq (proxyReq, req, res, options) {
+	var prefix = getPrefix(req);
+	if (req.body) {
+		var body = req.body;
+		try {
+			body = JSON.parse(body);
+
+			if (req.url.indexOf('_mget') !== -1) {
+				body = prepareMgetBody(body, prefix);
+			}
+
+			if (Object.keys(body).length) {
+				proxyReq.write(req.body);
+			} else {
+				proxyReq.write("");
+			}
+
+		} catch (e) {
+			if (typeof body === 'object') {
+				body = "{}";
+			}
+			proxyReq.write(body);
+
+		}
+	}
+}
+
+function guardProxy(req, res) {
+	var proxy=req.proxy;
+	var prefix = getPrefix(req);
+	// You can define here your custom logic to handle the request
+	// and then proxy the request.
+	//console.log(req.url);
+	if (prefix && req.url.indexOf('/elasticsearch/') != -1) {
+		var url = req.url;
+		var parts = url.split('/');
+		var searchIndex = parts.findIndex(function (el) {
+			if (el === 'elasticsearch') {
+				return true;
+			} else {
+				return false;
+			}
+		});
+		searchIndex += 1;
+		var indexName;
+		if (parts.length >= searchIndex) {
+			indexName = parts[searchIndex];
+
+			if (isIndexName(indexName)) {
+				indexName = prepareIndexName(indexName, prefix);
+				parts[searchIndex] = indexName;
+			}
+		}
+		url = parts.join('/');
+		req.url = url;
+	}
+	// proxy.on('proxyReq', function(proxyReq, req, res, options) {
+	// 	console.log(res);
+	// });
+	proxy.web(req, res, {proxyTimeout: 600000, target: config.elk_host});
+}
+
+
+module.exports={
+	onProxyReq:onProxyReq,
+	guardProxy:guardProxy
+}
--- index.js
+++ index.js
@@ -1,140 +1,56 @@
-var config=require('./config.json');
+var config = require('./config.json');
+var users=config.users;
 var express = require('express');
+var passport = require('passport');
+
+var BasicStrategy = require('passport-http').BasicStrategy;
+passport.use(new BasicStrategy(
+	function(username, password, done) {
+		if(username){
+			return done(null,{username:username,password:password});
+		}else{
+			return done(null,false);
+		}
+
+	}
+));
 
 var http = require('http'),
 	connect = require('connect'),
 	request = require('request'),
-	colors = require('colors'),
 	util = require('util'),
 	bodyParser = require('body-parser'),
 	httpProxy = require('http-proxy'),
 	proxy = httpProxy.createProxyServer({});
-function getPrefix(req) {
-	var prefix;
-	var auth = req.headers.authorization;
-	if (auth) {
-		auth = new Buffer(req.headers.authorization.split(" ")[1], 'base64').toString();
-		auth = auth.split(':');
-		var username = auth[0];
-		var users=config.users;
-		prefix=users[username];
-	}
-	return prefix;
-}
-function isIndexName(indexName) {
-	if (indexName[0] === '_') {
-		return false;
-	}
-	return true;
-}
-function isReplacementNeeded(index) {
-	if (index.indexOf('asteriskcdrmain-') !== -1) {
-		return true;
-	}
-	if (index.indexOf('asteriskcdr-') !== -1) {
-		return true;
-	}
-	if (index.indexOf('asteriskqueuelog-') !== -1) {
-		return true;
-	}
-	return false;
-
-}
-function prepareIndexName(oldIndexName, prefix) {
-
-	if (!isReplacementNeeded(oldIndexName)) {
-		return oldIndexName;
-	}
-	var indexNameParts = oldIndexName.split('-*');
-	indexNameParts[0] = indexNameParts[0] + '_' + prefix;
-	var indexName = indexNameParts.join('-*');
-	return indexName;
-}
-function prepareMgetBody(body, prefix) {
-	var docs = body.docs;
-	var newBody = {};
-	var newDocs = docs.map(function (el) {
-		el._index = prepareIndexName(el._index, prefix);
-		el._id = prepareIndexName(el._id, prefix)
-		return el;
-	});
-	newBody.docs = newDocs;
-	return newBody;
-}
-
-proxy.on('proxyReq', function (proxyReq, req, res, options) {
-	var prefix = getPrefix(req);
-	console.log('proxy req', req.url);
-	if (req.body) {
-		var body = req.body;
-		try {
-			body = JSON.parse(body);
-
-			if (req.url.indexOf('_mget') !== -1) {
-				body = prepareMgetBody(body, prefix);
-			}
-
-			console.log(body);
 
 
-			if (Object.keys(body).length) {
-				proxyReq.write(req.body);
-			} else {
-				proxyReq.write("");
-			}
+var guardproxy=require('./guardproxy');
 
-		} catch (e) {
-			if (typeof body === 'object') {
-				body = "{}";
-			}
-			proxyReq.write(body);
+var onProxyReq=guardproxy.onProxyReq;
+var guardProxy=guardproxy.guardProxy;
+proxy.on('proxyReq', onProxyReq);
 
-		}
-	}
-});
-
-
-var app = express()
-	.use(bodyParser.text({
+var app = express();
+app.use(bodyParser.text({
 		type: function () {
 			return true
 		}
-	}))
-	.use(function (req, res) {
-		var prefix = getPrefix(req);
+	}));
+app.use(function(req,res,next){
+	passport.authenticate('basic', function(err, user, info) {
+		req.user=user;
+		console.log('aaa',user);
+		return next();
+	})(req, res, next);
+})
+app.use(function(req,res,next){
+	req.proxy=proxy;
+	return next();
+})
 
-		// You can define here your custom logic to handle the request
-		// and then proxy the request.
-		//console.log(req.url);
-		if (prefix && req.url.indexOf('/elasticsearch/') != -1) {
-			var url = req.url;
-			var parts = url.split('/');
-			var searchIndex = parts.findIndex(function (el) {
-				if (el === 'elasticsearch') {
-					return true;
-				} else {
-					return false;
-				}
-			});
-			searchIndex += 1;
-			var indexName;
-			if (parts.length >= searchIndex) {
-				indexName = parts[searchIndex];
+app.use(guardProxy);
 
-				if (isIndexName(indexName)) {
-					indexName = prepareIndexName(indexName, prefix);
-					parts[searchIndex] = indexName;
-				}
-			}
-			url = parts.join('/');
-			req.url = url;
-		}
-		// proxy.on('proxyReq', function(proxyReq, req, res, options) {
-		// 	console.log(res);
-		// });
-		proxy.web(req, res, {proxyTimeout: 600000, target: config.elk_host});
-	});
 
 http.createServer(app).listen(config.port, function () {
-	console.log('Kibana Guard started on port '+config.port);
+	console.log('Kibana Guard started on port ' + config.port);
 });
